795d225c55efbd4d9ebaf1a192a9f17f2a01112d
Anca-Nicoleta Ciubotaru <anca@parquery.com>
2020-05-29 15:55:06 +0200

    changed typo in import

--- efficientdet/anchors.py
+++ efficientdet/anchors.py
@@ -336,7 +336,7 @@ def _generate_detections_tf(cls_outputs,
     scores = detections[:, 4]
     boxes = detections[:, :4]
 
-  image_size = utils.parse_image_size(image_size)
+  image_size = ed_utils.parse_image_size(image_size)
   detections = tf.stack([
       tf.cast(tf.tile(image_id, tf.shape(top_detection_idx)), tf.float32),
       tf.clip_by_value(boxes[:, 0], 0, image_size[0]) * image_scale,
